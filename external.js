81adab9adbc1ea01a0808f642c24c437e6066c2f
Gyanashree Giri <giri.gyanashree347@gmail.com>
2022-02-09 07:17:33 +0545

    Set the rules of the game

--- external.js
+++ external.js
@@ -13,5 +13,33 @@ function playerPlay() {
     return playerChoice.toLowerCase();
 }
 
+// rules of the game
+function playRound(playerSelection, computerSelection) {
+    const playerChoice = playerSelection();
+    const computerChoice = computerSelection();
+    if (playerChoice === computerChoice) {
+        console.log("It's a draw. Life is fair!")
+        return null;
+    }else if (playerChoice === "rock" && computerChoice === "paper") {
+        console.log("You Loose! Happens to the best of us!");
+        return "computer";
+    } else if (playerChoice === "rock" && computerChoice === "scissor"){
+        console.log("You Win! Go Celebrate!");
+        return "player";
+    } else if (playerChoice === "paper" && computerChoice === "rock"){
+        console.log("You Win! Go Celebrate!");
+        return "player";
+    } else if (playerChoice === "paper" && computerChoice === "scissor"){
+        console.log("You Loose! Happens to the best of us!");
+        return "computer";
+    } else if (playerChoice === "scissor" && computerChoice === "paper"){
+        console.log("You Win! Go Celebrate!");
+        return "player";
+    } else if (playerChoice === "scissor" && computerChoice === "rock"){
+        console.log("You Loose! Happens to the best of us!");
+        return "computer";
+    }    
 
-playerPlay();
+}
+
+console.log(playRound(playerPlay, computerPlay));
